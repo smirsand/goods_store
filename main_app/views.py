5e378de3cb370c6db89505832ceee9ff684e0354
Sergey <smirsand@mail.ru>
2023-09-03 22:37:09 +0300

    add code views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 
+from main_app.models import Product, Category
+
+
+def home(request):
+    home = Category.objects.all()
+    context = {
+        'object_list': home,
+        'title': 'Главная'
+    }
+    return render(request, "main_app/home.html", context)
+
 
 def product_page(request):
-    return render(request, 'main_app/product_page.html')
+    product_page = Product.objects.all()
+    context = {
+        'object_list': product_page
+    }
+    return render(request, "main_app/product_page.html", context)
+
+
+def contact(request):
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'main_app/contacts.html', context)
