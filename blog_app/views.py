52da7909254d8964ecbe68bfdadfa3875a73cdeb
Sergey <smirsand@mail.ru>
2023-09-07 11:41:53 +0300

    add code views.py

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,10 +1,17 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
 from blog_app.models import Blog
 
 
 class BlogCreateView(CreateView):
+    model = Blog
+    template_name = 'blog_app/blog_app_form.html'
+    fields = ('header', 'content', 'slug',)
+    success_url = reverse_lazy('main_app:home')
+
+
+class BlogUpdateView(UpdateView):
     model = Blog
     template_name = 'blog_app/blog_app_form.html'
     fields = ('header', 'content', 'slug',)
@@ -19,3 +26,9 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Blog
     template_name = 'blog_app/blog_app_detail.html'
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    template_name = 'blog_app/blog_app_confirm_delete.html'
+    success_url = reverse_lazy('blog_app:list')
