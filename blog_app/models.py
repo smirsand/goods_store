5610e4de6ed689d80fd799c745473748a9b362c6
Sergey <smirsand@mail.ru>
2023-09-10 21:42:18 +0300

    add code models.py

--- blog_app/models.py
+++ blog_app/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Blog(models.Model):
     header = models.CharField(max_length=100, verbose_name='заголовок')
-    slug = models.CharField(max_length=100, unique=True, verbose_name='slug')
+    slug = models.CharField(max_length=150, verbose_name='slug', blank=True, null=True)
     content = models.TextField(verbose_name='content')
     image = models.ImageField(verbose_name='изображение', blank=True, null=True)
     date_of_creation = models.DateTimeField(verbose_name='дата создания', blank=True, null=True)
-    sign_of_publication = models.BooleanField(verbose_name='признак публикации', blank=True, null=True)
-    number_of_views = models.IntegerField(verbose_name='количество просмотров', blank=True, null=True)
+    sign_of_publication = models.BooleanField(default=True, verbose_name='признак публикации', blank=True, null=True)
+    number_of_views = models.IntegerField(default=0, verbose_name='количество просмотров')
 
     def __str__(self):
         return self.header
