2d2ea60aad232c1bf31d96c5dafd5b1671d3a244
Sergey <smirsand@mail.ru>
2023-09-19 21:22:05 +0300

    add code admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from main_app.models import Category, Product
+from main_app.models import Category, Product, Version
 
 
 @admin.register(Category)
@@ -10,6 +10,19 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'product_name', 'price', 'category',)
+    list_display = ('pk', 'product_name', 'price', 'category', 'active_version')
     list_filter = ('category',)
     search_fields = ('product_name', 'description',)
+
+    def active_version(self, obj):
+        active = Version.objects.get(product=obj, version_flag=True)
+
+        return active.version_flag
+
+    active_version.short_description = 'Признак версии'
+
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'product', 'version_name', 'version_flag')
+    list_filter = ('version_flag',)
