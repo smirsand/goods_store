1eb9f530ad3c37e077aff76224ab6ae164a18bf6
Sergey <smirsand@mail.ru>
2023-09-19 21:23:58 +0300

    add code forms.py

--- main_app/forms.py
+++ main_app/forms.py
@@ -4,10 +4,14 @@ from main_app.models import Product
 
 
 class ProductForm(forms.ModelForm):
-
     class Meta:
         model = Product
         fields = ('product_name', 'description', 'price', 'category', 'image')
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            for field_name, field in self.fields.items():
+                field.widget.attrs['class'] = 'form-control'
 
     def clean_product_name(self):
         cleaned_data = self.cleaned_data['product_name']
