81815df3a84f44a8866ba035dd02b495a9931d74
Sergey <smirsand@mail.ru>
2023-09-20 17:00:51 +0300

    add views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,10 @@
+from django.forms import inlineformset_factory
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
-from main_app.forms import ProductForm
-from main_app.models import Category, Product
+from main_app.forms import ProductForm, VersionForm
+from main_app.models import Category, Product, Version
 
 
 class CategoryListView(ListView):
@@ -43,3 +44,24 @@ class ProductUpdateView(UpdateView):
     form_class = ProductForm
     template_name = 'main_app/product_form.html'
     success_url = reverse_lazy('main_app:product_page')
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        if self.request.method == 'POST':
+            formset = VersionFormset(self.request.POST, instance=self.object)
+        else:
+            formset = VersionFormset(instance=self.object)
+
+        context_data['formset'] = formset
+        return context_data
+
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        self.object = form.save()
+
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+        return super().form_valid(form)
