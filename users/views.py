fac69dfea6c33eeae94a5d1d2b163c6ab82237a8
Sergey <smirsand@mail.ru>
2023-09-24 16:45:11 +0300

    add code views.py users

--- users/views.py
+++ users/views.py
@@ -1,4 +1,9 @@
-from django.urls import reverse_lazy
+import random
+
+from django.conf import settings
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView
 
 from users.forms import UserRegisterForm, UserProfileForm
@@ -11,11 +16,34 @@ class RegisterView(CreateView):
     template_name = "users/register.html"
     success_url = reverse_lazy("users:login")
 
+    def form_valid(self, form):
+        new_user = form.save()
+        send_mail(
+            subject='Поздравляем с регистрацией!',
+            message='Вы зарегистрировались на нашей форме, добро пожаловать!',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[new_user.email]
+        )
+        return super().form_valid(form)
+
 
 class ProfileView(UpdateView):
     model = User
     form_class = UserProfileForm
     success_url = reverse_lazy("users:profile")
 
-    def get_object(self, queryset=None):
+    def get_object(self, queryset=None):  # Для редактирования текущего пользователя.
         return self.request.user
+
+
+def generate_new_password(request):
+    new_password = ''.join([str(random.randint(0, 9)) for _ in range(12)])
+    send_mail(
+        subject='Вы сменили пароль!',
+        message=f'Ваш новый пароль: {new_password}',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[request.user.email]
+    )
+    request.user.set_password(new_password)
+    request.user.save()
+    return redirect(reverse('main_app:home'))
