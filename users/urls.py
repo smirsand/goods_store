5cc72c1a3225afc9be863f131c66776e3aa491f1
Sergey <smirsand@mail.ru>
2023-09-25 13:01:29 +0300

    add code urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView, generate_new_password, ResetPasswordView, LoginView, LogoutView
+from users.views import RegisterView, ProfileView, generate_new_password, LoginView, LogoutView
 
 app_name = UsersConfig.name
 
+
 urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
+
     path('logout/', LogoutView.as_view(), name='logout'),
 
     path('register/', RegisterView.as_view(), name='register'),
@@ -15,5 +17,5 @@ urlpatterns = [
 
     path('profile/genpassword/', generate_new_password, name='generate_new_password'),
 
-    path('password_reset/', ResetPasswordView.as_view(), name='password_reset'),
+    # path('password_reset/', ResetPasswordView.as_view(), name='password_reset'),
 ]
